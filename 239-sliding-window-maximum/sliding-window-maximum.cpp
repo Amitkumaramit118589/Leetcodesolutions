9c5a354de6e1ad0bfd31d170d3856d8a51ac09cb
amit kumar <amitkumaramit118589@gmail.com>
2025-08-16 01:11:07 +0530

    Time: 23 ms (61.37%) | Memory: 142.6 MB (23.64%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.cpp
+++ 239-sliding-window-maximum/sliding-window-maximum.cpp
@@ -1,30 +1,31 @@
 class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-        int n = nums.size();
-        vector<int> nextGreater(n);
-        stack<int> st;
-
-        // Compute next greater element indices
-        for (int i = n - 1; i >= 0; i--) {
-            while (!st.empty() && nums[st.top()] <= nums[i]) {
+        int n=nums.size();
+        if(k<=1)return nums;
+        stack<int>st;
+        st.push(n-1);
+        vector<int>ngi(n);
+        ngi[n-1]=n;
+        for(int i=n-2;i>=0;i--){
+            while(!st.empty() && nums[i]>=nums[st.top()]){
                 st.pop();
             }
-            nextGreater[i] = st.empty() ? n : st.top();
+            if(st.empty()) ngi[i]=n;
+            else ngi[i]=st.top();
             st.push(i);
         }
-
-        int j = 0;
-        vector<int> res(n - k + 1);
-
-        for (int i = 0; i <= n - k; i++) {
+        
+        //next greater ke hisab se window me check karna aur jump karna
+        vector<int> ans;
+        int j=0;
+        for(int i=0;i<=n-k;i++){
             if (j < i) j = i;
-            while (nextGreater[j] < i + k) {
-                j = nextGreater[j];
+            while(j<n && ngi[j]<i+k){
+                j=ngi[j];
             }
-            res[i] = nums[j];
+            ans.push_back(nums[j]);
         }
-
-        return res;
+        return ans;
     }
 };
