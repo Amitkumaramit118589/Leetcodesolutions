2edd36a37034e05a682756a1ba2593820c5ba135
amit kumar <amitkumaramit118589@gmail.com>
2025-08-28 23:25:26 +0530

    Time: 31 ms (21.01%) | Memory: 11 MB (56.61%) - LeetSync

--- 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.cpp
+++ 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.cpp
@@ -5,6 +5,7 @@ public:
         int i = 0;
         int j = 0;
         int count = 0;
+        int result=0;
         unordered_map<char, int> mp;
 
         while (j < n) {
@@ -12,15 +13,14 @@ public:
 
             // Shrink the window until it contains all three characters
             while (mp['a'] > 0 && mp['b'] > 0 && mp['c'] > 0) {
-                count += (n - j); // Add all substrings ending at j
-                mp[s[i]]--;       // Shrink the window from the left
-                if (mp[s[i]] == 0) {
-                    mp.erase(s[i]); // Remove character from map if count is 0
-                }
+               
+                mp[s[i]]--;       
+                
                 i++;
             }
+            result+=i;
             j++;
         }
-        return count;
+        return result;
     }
 };
