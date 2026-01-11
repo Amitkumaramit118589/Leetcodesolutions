0c74b3ca8d4779fb854871ed75109f92eeb5d8d2
amit kumar <amitkumaramit118589@gmail.com>
2026-01-11 20:36:37 +0530

    Time: 14 ms (18.30%) | Memory: 20.4 MB (30.53%) - LeetSync

--- 85-maximal-rectangle/maximal-rectangle.cpp
+++ 85-maximal-rectangle/maximal-rectangle.cpp
@@ -1,46 +1,56 @@
 class Solution {
 public:
-    int maximalRectangle(vector<vector<char>>& matrix) {
-        if (matrix.empty() || matrix[0].empty()) return 0;
+    int largestRectangleArea(vector<int>& heights) {
+        int n = heights.size();
+        stack<int> st;
+        st.push(-1);
 
-        int cols = matrix[0].size();
-        vector<int> heights(cols, 0);
+        vector<int> prevSmaller(n), nextSmaller(n);
+
+        for (int i = 0; i < n; i++) {
+            while (st.top() != -1 && heights[st.top()] >= heights[i])
+                st.pop();
+            prevSmaller[i] = st.top();
+            st.push(i);
+        }
+
+        while (!st.empty()) st.pop();
+        st.push(-1);
+
+        for (int i = n - 1; i >= 0; i--) {
+            while (st.top() != -1 && heights[st.top()] >= heights[i])
+                st.pop();
+            nextSmaller[i] = (st.top() == -1) ? n : st.top();
+            st.push(i);
+        }
+
         int maxArea = 0;
-
-        for (auto& row : matrix) {
-            for (int i = 0; i < cols; ++i)
-                heights[i] = (row[i] == '1') ? heights[i] + 1 : 0;
-            maxArea = max(maxArea, largestRectangleArea(heights));
+        for (int i = 0; i < n; i++) {
+            int height = heights[i];
+            int width = nextSmaller[i] - prevSmaller[i] - 1;
+            maxArea = max(maxArea, height * width);
         }
 
         return maxArea;
     }
 
-    int largestRectangleArea(vector<int>& heights) {
-        int n = heights.size();
-        vector<int> left(n), right(n, n);
-        stack<int> st;
+    int maximalRectangle(vector<vector<char>>& matrix) {
+        int rows = matrix.size();
+        int cols = matrix[0].size();
 
-        for (int i = 0; i < n; ++i) {
-            while (!st.empty() && heights[st.top()] >= heights[i])
-                st.pop();
-            left[i] = st.empty() ? -1 : st.top();
-            st.push(i);
+        vector<int> histogram(cols, 0);
+        int answer = 0;
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (matrix[i][j] == '1')
+                    histogram[j]++;
+                else
+                    histogram[j] = 0;
+            }
+            answer = max(answer, largestRectangleArea(histogram));
         }
 
-        st = stack<int>();
-
-        for (int i = n - 1; i >= 0; --i) {
-            while (!st.empty() && heights[st.top()] >= heights[i])
-                st.pop();
-            right[i] = st.empty() ? n : st.top();
-            st.push(i);
-        }
-
-        int maxArea = 0;
-        for (int i = 0; i < n; ++i)
-            maxArea = max(maxArea, heights[i] * (right[i] - left[i] - 1));
-
-        return maxArea;
+        return answer;
     }
 };
