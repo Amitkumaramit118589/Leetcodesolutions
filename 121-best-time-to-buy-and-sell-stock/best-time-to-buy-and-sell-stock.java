6cf552bdc830fb0dcfae75c9e27a47eb5ae01ae7
amit kumar <amitkumaramit118589@gmail.com>
2026-01-21 23:13:50 +0530

    Time: 2 ms (42.08%) | Memory: 94.4 MB (76.41%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.java
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.java
@@ -5,8 +5,8 @@ class Solution {
         for (int i = 1; i < prices.length; i++) {
             if (prices[i] < buy) {
                 buy = prices[i];
-            } else if (prices[i] - buy > profit) {
-                profit = prices[i] - buy;
+            } else {
+                profit =Math.max(profit, prices[i] - buy);
             }
         }
         return profit;
