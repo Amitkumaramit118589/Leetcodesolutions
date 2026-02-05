052af1b2ff31ce2100f726d29b82355f2fd5bee8
amit kumar <amitkumaramit118589@gmail.com>
2026-02-06 00:36:23 +0530

    Time: 1 ms (59.81%) | Memory: 47.5 MB (46.26%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/set-matrix-zeroes.java
@@ -1,30 +1,32 @@
 class Solution {
+    void setcolumn(int[][] matrix , int idx){
+        for(int i=0;i<matrix.length;i++){
+            if(matrix[i][idx]!=0) matrix[i][idx]=Integer.MAX_VALUE-10;
+        }
+    }
+    void setrow(int[][] matrix , int idx){
+        for(int i=0;i<matrix[0].length;i++){
+            if(matrix[idx][i]!=0) matrix[idx][i]=Integer.MAX_VALUE-10;
+        }
+    }
     public void setZeroes(int[][] matrix) {
-        int m = matrix.length;
-        int n = matrix[0].length;
-        int row[] = new int[m];
-        int column[] = new int[n];
-        for(int i=0; i<m;i++){
-            for(int j=0; j<n; j++){
-                if(matrix[i][j] == 0){
-                    row[i] = 1;
-                    column[j] = 1;
+    
+        for(int i=0;i<matrix.length;i++){
+            for(int j=0;j<matrix[0].length;j++){
+                if(matrix[i][j]==0){
+                    setrow(matrix,i);
+                    setcolumn(matrix,j);
                 }
             }
         }
-        for(int i = 0; i<m; i++){
-            if(row[i] == 1){
-                for(int j=0; j<n; j++){
-                    matrix[i][j] = 0;
+         for(int i=0;i<matrix.length;i++){
+            for(int j=0;j<matrix[0].length;j++){
+                if(matrix[i][j]==Integer.MAX_VALUE-10){
+                    matrix[i][j]=0;
                 }
             }
         }
-        for(int j=0; j<n; j++){
-            if(column[j] == 1){
-                for(int i=0; i<m; i++){
-                    matrix[i][j] = 0;
-                }
-            }
-        }
+
+        
     }
 }
